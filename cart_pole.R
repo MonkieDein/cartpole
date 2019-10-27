acbbc83729a0a3507a2f577408823caa6d7c77de
Jia Lin Hau <jh1111@wildcats.unh.edu>
2019-10-27 18:08:05 -0400

    Initial commit

---
+++ cart_pole.R
@@ -0,0 +1,5 @@
+setwd("~/Desktop/AML/cartpole")
+# Moth_M_spray = read.csv("MothM_Spray.csv")
+df = read.csv("cartpole.csv")
+
+
---
+++ marek_cart_pole.py
@@ -0,0 +1,44 @@
+
+## Generate samples
+
+import random
+import gym
+import tqdm
+import csv
+env = gym.make('CartPole-v1')
+
+generate_samples = True
+
+random.seed(2019)
+
+if(generate_samples):
+    print("Generating samples ...")
+
+    with open('cartpole.csv', 'w', newline='') as csvfile:
+        samplewriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+
+        samplewriter.writerow(["Step", "CartPos", "CartVelocity", "PoleAngle", \
+                                "PoleVelocity", "Action", "Reward"])
+        
+        laststate = None
+        for k in tqdm.trange(2000):
+            env.reset()
+            done = False
+            count = 0
+            for i in range(200):
+                env.render()
+                action = env.action_space.sample()
+                if i > 0:
+                    samplewriter.writerow((i-1,) + tuple(state) + (action,) + (reward,))
+                
+                # stop only after saving the state
+                if count ==1:
+                  break
+                if done:
+                  count = 1
+                    #time.sleep(0.5)
+                    # break
+                [state,reward,done,info] = env.step(action) # take a random action
+                
+                
+    env.close()
